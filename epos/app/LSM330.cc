246d42866fab8e6660e8bdcde91b60f9864fc483
Juliano Zatta <julianokzatta@gmail.com>
2016-10-30 23:19:08 -0200

    Compilled and running in epos, USB working

--- epos/app/LSM330.cc
+++ epos/app/LSM330.cc
@@ -1,71 +1,79 @@
+
 #include <machine.h>
 #include <alarm.h>
 #include <utility/ostream.h>
 #include <i2c.h>
 #include <gpio.h>
 #include <cpu.h>
-#include <machine/cortex_m/emote3_gptm.h>
 
 using namespace EPOS;
 
 OStream cout;
 
-class LSM330
-{
-private:
-    //Addresses
-    enum Accelerometer {
-        
-    };
-    
-    enum Gyroscope {
-        
-    };
-
-public:
-
-    LSM330() {
-        _i2c =  new I2C(MASTER, 'B', 1, 'B', 0); //master, SDA = PB1, SCL = PB0
-    }
-
-    int reset(void) {
-        return 0;
-    }
-
-    int getAngleX() {
-        return 0;
-    }
-
-    int getAngleY() {
-        return 0;
-    }
-
-    int getAngleZ() {
-        return 0;
-    }
-
-    int getAccelerationX() {
-        return 0;
-    }
-
-    int getAccelerationY() {
-        return 0;
-    }
-
-    int getAccelerationZ() {
-        return 0;
-    }
-
-private:
-    I2C * _i2c;
-};
+// class LSM330
+// {
+// private:
+//     //Addresses
+//     enum Accelerometer {
+//         
+//     };
+//     
+//     enum Gyroscope {
+//         
+//     };
+// 
+// public:
+// 
+//     LSM330() {
+//         _i2c =  new I2C(MASTER, 'B', 1, 'B', 0); //master, SDA = PB1, SCL = PB0
+//     }
+// 
+//     int reset(void) {
+//         return 0;
+//     }
+// 
+//     float getAngleX() {
+//         return 0;
+//     }
+// 
+//     float getAngleY() {
+//         return 0;
+//     }
+// 
+//     float getAngleZ() {
+//         return 0;
+//     }
+// 
+//     float getAccelerationX() {
+//         return 0;
+//     }
+// 
+//     float getAccelerationY() {
+//         return 0;
+//     }
+// 
+//     float getAccelerationZ() {
+//         return 0;
+//     }
+// 
+// private:
+//     I2C * _i2c;
+// };
 
 int main()
 {
-    LSM330 gyro;
-    cout << "I2C Test\n";
+//     LSM330 gyro;
+    cout << "I2C test" << endl;
+    GPIO g('C',3, GPIO::OUTPUT);
+    
+    cout << "Start" << endl;
     while(1) {
-
+        Delay(100000);
+        cout << "Set" << endl;
+        g.set(1);
+        Delay(100000);
+        cout << "reset" << endl;
+        g.set(0);
     }
     return 0;
 }
